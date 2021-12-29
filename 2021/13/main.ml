cdd572ef8f6263635c10fad92628fe5bb0fcfe8a
Ahmad Rasekh <ahmd.rasekh@gmail.com>
2021-12-29 02:03:19 +0000

    day 13 cleanup

--- 2021/13/main.ml
+++ 2021/13/main.ml
@@ -12,18 +12,18 @@ let parse_input () = "./input" |>
                      List.map parse_line
 
 let folds = [
-(655, 0);
-(0, 447);
-(327, 0);
-(0, 223);
-(163, 0);
-(0, 111);
-(81, 0);
-(0, 55);
-(40, 0);
-(0, 27);
-(0, 13);
-(0, 6 )
+  (655, 0);
+  (0, 447);
+  (327, 0);
+  (0, 223);
+  (163, 0);
+  (0, 111);
+  (81, 0);
+  (0, 55);
+  (40, 0);
+  (0, 27);
+  (0, 13);
+  (0, 6 )
 ]
 
 let fold_x x = 
@@ -51,10 +51,27 @@ let rec fold  instructions input = match instructions with
   | (0, y)::tl -> let input' = fold_y y input in fold tl input'
   | (x, 0)::tl -> let input' = fold_x x input in fold tl input'
 
+let plot (input:t) =
+  let (x, y) = input |> 
+               List.split |>
+               fun (xs, ys) -> 
+               (
+                 xs |> List.sort_uniq compare |> List.rev |> List.hd |> (+) 1,
+                 ys |> List.sort_uniq compare |> List.rev |> List.hd |> (+) 1
+               ) in
+  Array.make y (fun () -> (Array.make x '.')) |> 
+  Array.map (fun x -> x () ) |> 
+  (fun canvas -> 
+     List.iter (fun (x, y) -> canvas.(y).(x) <- '0') input;
+     canvas |>
+     Array.map (Array.to_seq >> String.of_seq) |>
+     Array.iter print_endline)
+
 let () = parse_input () |>
          fold folds |>
-         List.sort_uniq compare |>
-         sexp_of_t |>
-        Core.Sexp.to_string |>
-        print_endline
+         List.sort_uniq compare |> 
+         plot
+(* sexp_of_t |>
+   Core.Sexp.to_string |>
+   print_endline *)
 
